4b105676ba04fbed6caa5987b4f691b9b3557b72
Ruben Manuel Rodriguez Chamorro <rrodrcha@emeal.nttdata.com>
2026-05-11 12:14:08 +0200

    feat(operacion-service): implementa depositos, retiros y transferencias #12

--- operacion-service/src/main/java/com/novabank/operacion/service/OperacionService.java
+++ operacion-service/src/main/java/com/novabank/operacion/service/OperacionService.java
@@ -1,4 +1,14 @@
 package com.novabank.operacion.service;
 
-public class opserv {
+import com.novabank.operacion.dto.DepositoRequest;
+import com.novabank.operacion.dto.RetiroRequest;
+import com.novabank.operacion.dto.TransferenciaRequest;
+
+public interface OperacionService {
+
+    void realizarDeposito(DepositoRequest request);
+
+    void realizarRetiro(RetiroRequest request);
+
+    void realizarTransferencia(TransferenciaRequest request);
 }
